be44243d05c387278973d07afddf854303f4773e
longpc5 <longnuyn392@gmail.com>
2024-07-14 18:53:08 +0700

    Done MaxSubArray, still slow :((

--- Array/MaxSubarray/src/main/java/org/example/Main.java
+++ Array/MaxSubarray/src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
 public class Main {
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int sum = 0;
+
+        if (nums.length == 1)
+            return nums[0];
+
+        for (int num : nums) {
+            if (sum >= 0)
+                sum += num;
+            else
+                sum = num;
+
+            if (sum > maxSum)
+                maxSum = sum;
+        }
+
+        return maxSum;
+    }
+
+    public static int anotherFasterMaxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int sum = maxSum;
+
+        for (int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        return maxSum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {5,4,-1,7,8};
+        System.out.print(maxSubArray(nums));
     }
 }
