f7a52a30da5c2b2e977695cbd9bfd4689f6838a6
longpc5 <longnuyn392@gmail.com>
2024-07-16 16:12:01 +0700

    Done MaxPSubArray

--- Array/MaxProductSubArray/src/main/java/org/example/Main.java
+++ Array/MaxProductSubArray/src/main/java/org/example/Main.java
@@ -2,18 +2,28 @@ package org.example;
 
 public class Main {
     public static int maxProduct(int[] nums) {
-        int maxProd = nums[0];
-        int prod = maxProd;
+        int n = nums.length;
+        double leftProd = 1;
+        double rightProd = 1;
+        double result = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {
-            prod = Math.max(prod * nums[i], nums[i]);
-            maxProd = Math.max(maxProd, prod);
+        for (int i = 0; i < n; i++) {
+            if (leftProd == 0)
+                leftProd = 1;
+            if (rightProd == 0)
+                rightProd = 1;
+
+            leftProd *= nums[i];
+            rightProd *= nums[n - i - 1];
+
+            result = Math.max(result, Math.max(leftProd, rightProd));
         }
 
-        return maxProd;
+        return (int)result;
     }
     public static void main(String[] args) {
-        int[] nums = {-2,0,-1};
+        int[] nums = {0,10,10,10,10,10,10,10,10,10,-10,10,10,10,10,10,10,10,10,10,0};
         System.out.print(maxProduct(nums));
+        // System.out.print(10 * 10 * 10 * -10);
     }
 }
