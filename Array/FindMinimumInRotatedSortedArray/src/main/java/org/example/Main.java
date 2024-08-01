36197059820a4269b6fe58f1ce36aad87a981b1d
longpc5 <longnuyn392@gmail.com>
2024-08-01 18:38:29 +0700

    Using BinaryTree Algorithm but Why is it correct???

--- Array/FindMinimumInRotatedSortedArray/src/main/java/org/example/Main.java
+++ Array/FindMinimumInRotatedSortedArray/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
 public class Main {
+    public static int findMin(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] > arr[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return arr[left];
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int result = findMin(nums);
+
+        System.out.print(result);
     }
 }
